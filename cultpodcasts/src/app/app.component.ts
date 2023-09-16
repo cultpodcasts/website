508f4000a49b5ef6cb98e406a9d70dc2291b4583
Jon Breen <jonbreen@outlook.com>
2023-09-16 19:10:14 +0100

    Added timer and results-summary

--- cultpodcasts/src/app/app.component.ts
+++ cultpodcasts/src/app/app.component.ts
@@ -15,15 +15,25 @@ export class AppComponent {
 
   results: any;
   title = 'cultpodcasts';
+  resultsHeading: string= "";
 
   search= (input:HTMLInputElement) => {
     input.blur();
     this.isLoading= true;
     let url= "https://api.cultpodcasts.com/api/episode_search/"+encodeURIComponent(input.value);
+    let currentTime= Date.now();
     this.http.get(url)
       .subscribe(data => {
         this.results= data;
         this.isLoading= false;
+        var requestTime= (Date.now() - currentTime)/1000;
+        if (this.results.length===0) {
+          this.resultsHeading= "0 Results. Time taken "+requestTime+" seconds."
+        } else if (this.results.length===1) {
+          this.resultsHeading= "1 Results. Time taken "+requestTime+" seconds."
+        } else {
+          this.resultsHeading= this.results.length+" Results. Time taken "+requestTime+" seconds."
+        } 
       }, error=>{this.isLoading= false});
   };
 }
