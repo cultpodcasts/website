ae2c1a437d64d293a45fb7ff311083313ea1be4c
Jon Breen <jonbreen@outlook.com>
2023-09-30 18:35:24 +0100

    Service icons

--- cultpodcasts/src/app/app.component.ts
+++ cultpodcasts/src/app/app.component.ts
@@ -16,6 +16,9 @@ export class AppComponent {
     this.iconRegistry.addSvgIcon(`reddit`, this.domSanitizer.bypassSecurityTrustResourceUrl("../assets/reddit.svg"));
     this.iconRegistry.addSvgIcon(`twitter`, this.domSanitizer.bypassSecurityTrustResourceUrl("../assets/twitter.svg"));
     this.iconRegistry.addSvgIcon(`github`, this.domSanitizer.bypassSecurityTrustResourceUrl("../assets/github.svg"));
+    this.iconRegistry.addSvgIcon(`spotify`, this.domSanitizer.bypassSecurityTrustResourceUrl("../assets/spotify.svg"));
+    this.iconRegistry.addSvgIcon(`youtube`, this.domSanitizer.bypassSecurityTrustResourceUrl("../assets/youtube.svg"));
+    this.iconRegistry.addSvgIcon(`apple-podcasts`, this.domSanitizer.bypassSecurityTrustResourceUrl("../assets/apple-podcasts.svg"));
   }
   
   search= (input:HTMLInputElement) => {
