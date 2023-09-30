c92ea90a359961ffc60a3ece012e67e3035c7b94
CultPodcastsBot <142722442+cultpodcasts@users.noreply.github.com>
2023-09-30 18:11:57 +0100

    Removed duplicate routes for same component (#18)
    Co-authored-by: Jon Breen <jonbreen@outlook.com>

--- cultpodcasts/src/app/search/search.component.ts
+++ cultpodcasts/src/app/search/search.component.ts
@@ -1,7 +1,8 @@
 import { Component, inject } from '@angular/core';
 import { ISearchResult } from '../ISearchResult';
 import { HttpClient } from '@angular/common/http';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Data, Params } from '@angular/router';
+import { combineLatest } from 'rxjs/internal/observable/combineLatest';
 
 @Component({
   selector: 'app-search',
@@ -11,50 +12,62 @@ import { ActivatedRoute } from '@angular/router';
 
 export class SearchComponent {
   query: string="";
-  order: string|undefined;
   prevPage: number=0;
   nextPage: number=0;
+  page:number|undefined;
   constructor(private http: HttpClient) {}
-  private activatedRoute = inject(ActivatedRoute);
+  private route = inject(ActivatedRoute);
   
   results: any;
   resultsHeading: string= "";
   isLoading: boolean= true;
   showPagingPrevious:boolean=false;
+  showPagingPreviousInit: boolean= false;
   showPagingNext:boolean=false;
 
   ngOnInit() {
-    this.activatedRoute.params.subscribe(({query, page, order}) =>{
+    combineLatest(
+      this.route.params,
+      this.route.data,
+      (params: Params, data: Data) => ({
+        params,
+        data,
+      })
+    ).subscribe((res: { params: Params; data: Data }) => {
+      const { params, data} = res;
+
       this.isLoading= true;
-      this.query= query;
-      if (!page) {
-        page= 1;
+      this.query= params["query"];
+
+      if (params["page"]) {
+        this.page= parseInt(params["page"]);
+        this.prevPage= this.page - 1;
+        this.nextPage=  this.page + 1;
+      } else {
+        this.nextPage= 2;
+        this.page= undefined;
       }
 
-      if (page) {
-        this.prevPage= parseInt(page) - 1;
-        this.nextPage=  parseInt(page) + 1;
+      let url= "https://api.cultpodcasts.com/api/episode_search/"+encodeURIComponent(this.query);
+      if (this.page) {
+        url= url+"?page="+this.page;
       }
 
-      this.order= order;
-      let url= "https://api.cultpodcasts.com/api/episode_search/"+encodeURIComponent(query);
-      if (page && parseInt(page)>1) {
-        url= url+"?page="+parseInt(page);
-      }
       let currentTime= Date.now();
       this.http.get<ISearchResult[]>(url)
         .subscribe(data => {
           this.results= data;
           var requestTime= (Date.now() - currentTime)/1000;
           if (this.results.length===0) {
-            this.resultsHeading= `Found 0 results for "${query}". Time taken ${requestTime} seconds.`;
+            this.resultsHeading= `Found 0 results for "${this.query}". Time taken ${requestTime} seconds.`;
           } else if (this.results.length===1) {
-            this.resultsHeading= `Found 1 result for "${query}". Time taken ${requestTime} seconds.`;
+            this.resultsHeading= `Found 1 result for "${this.query}". Time taken ${requestTime} seconds.`;
           } else {
-            this.resultsHeading= `Found ${this.results.length} results for "${query}". Time taken ${requestTime} seconds.`;
+            this.resultsHeading= `Found ${this.results.length} results for "${this.query}". Time taken ${requestTime} seconds.`;
           } 
           this.isLoading= false;
-          this.showPagingPrevious= page > 1;
+          this.showPagingPrevious= this.page!=undefined && this.page > 2;
+          this.showPagingPreviousInit= this.page!=undefined && this.page == 2;
           this.showPagingNext= this.results.length==100;
         }, error=>{
           this.resultsHeading= "Something went wrong. Please try again.";
