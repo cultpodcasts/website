b1760a5cc11e0f1cc3b2b26d929ddedc81a261e9
Jon Breen <jonbreen@outlook.com>
2023-09-30 15:19:00 +0100

    Fixed menu

--- cultpodcasts/src/app/app.component.ts
+++ cultpodcasts/src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import {Router} from '@angular/router';
+import { MatIconRegistry } from "@angular/material/icon";
+import { DomSanitizer } from "@angular/platform-browser";
 
 @Component({
   selector: 'app-root',
@@ -9,8 +11,13 @@ import {Router} from '@angular/router';
 })
 
 export class AppComponent {
-  constructor(private http: HttpClient, private router: Router) {}
-
+  constructor(private http: HttpClient, private router: Router, private iconRegistry:MatIconRegistry,
+    private domSanitizer: DomSanitizer) {
+    this.iconRegistry.addSvgIcon(`reddit`, this.domSanitizer.bypassSecurityTrustResourceUrl("../assets/reddit.svg"));
+    this.iconRegistry.addSvgIcon(`twitter`, this.domSanitizer.bypassSecurityTrustResourceUrl("../assets/twitter.svg"));
+    this.iconRegistry.addSvgIcon(`github`, this.domSanitizer.bypassSecurityTrustResourceUrl("../assets/github.svg"));
+  }
+  
   search= (input:HTMLInputElement) => {
     input.blur();
     this.router.navigate(['/search/'+input.value]);
