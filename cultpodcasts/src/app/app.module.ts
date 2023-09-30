df510dc5572895780a759214e30e13f3467fb063
CultPodcastsBot <142722442+cultpodcasts@users.noreply.github.com>
2023-09-30 15:21:58 +0100

    Fixed menu (#16)
    Co-authored-by: Jon Breen <jonbreen@outlook.com>

--- cultpodcasts/src/app/app.module.ts
+++ cultpodcasts/src/app/app.module.ts
@@ -16,6 +16,7 @@ import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatProgressBarModule} from '@angular/material/progress-bar';
 import { SearchComponent } from './search/search.component';
 import { HomeComponent } from './home/home.component';
+import {MatMenuModule} from '@angular/material/menu';
 
 
 @NgModule({
@@ -37,7 +38,8 @@ import { HomeComponent } from './home/home.component';
     MatCardModule,
     MatExpansionModule,
     MatToolbarModule,
-    MatProgressBarModule
+    MatProgressBarModule,
+    MatMenuModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
