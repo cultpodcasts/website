0b5be0ce090e52f6068428f0ed4658b5dd005498
Jon Breen <jonbreen@outlook.com>
2023-10-17 17:35:55 +0100

    Grouped homepage items

--- cultpodcasts/src/app/home/home.component.ts
+++ cultpodcasts/src/app/home/home.component.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { IHomepage } from '../IHomepage';
 import { SiteService } from '../SiteService';
+import { IHomepageItem } from '../IHomepageItem';
+import { KeyValue } from '@angular/common';
+
 
 @Component({
   selector: 'app-home',
@@ -9,9 +12,33 @@ import { SiteService } from '../SiteService';
   styleUrls: ['./home.component.sass']
 })
 export class HomeComponent {
-  constructor(private http: HttpClient, private siteService: SiteService) {}
+  grouped: { [key: string]: IHomepageItem[]; } ;
+  constructor(private http: HttpClient, private siteService: SiteService) {
+    this.grouped= {};
+  }
 
-  homepage: IHomepage|undefined;
+  Weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
+  Month = ["January","February","March","April","May","June","July", "August", "September", "October", "November", "December"]
+
+  ToDate = (dateStr:string) => {
+    const [day, month, year] = dateStr.split("/")
+    return new Date(parseInt(year), parseInt(month) - 1, parseInt(day))
+  }
+
+  descDate = (a: KeyValue<string, IHomepageItem[]>, b: KeyValue<string, IHomepageItem[]>):number => {
+
+    var aD= this.ToDate(a.key);
+    var bD= this.ToDate(b.key);
+    if (aD > bD) {
+      return -1;
+    }
+    if (aD < bD) {
+      return 1
+    }
+    return 0;
+  }
+
+ homepage: IHomepage|undefined;
   totalDuration: string="";
 
   ngOnInit() {
@@ -20,6 +47,14 @@ export class HomeComponent {
       .subscribe(data=>{
         this.homepage= data;
         this.totalDuration= data.totalDuration.split(".")[0]+" days";
-      });
+        this.grouped= data.recentEpisodes.reduce((group: {[key: string]: IHomepageItem[]}, item)=>{
+          item.release= new Date(item.release);
+          if (!group[item.release.toLocaleDateString()]) {
+            group[item.release.toLocaleDateString()] = [];
+           }
+           group[item.release.toLocaleDateString()].push(item);
+           return group;
+        }, {});
+    });
   }
 }
