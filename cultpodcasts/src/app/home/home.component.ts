e34051617b5eb46f08f92feaf24168ab2998b7e3
Jon Breen <jonbreen@outlook.com>
2023-10-19 22:17:52 +0100

    No more homepage doom-scrolling

--- cultpodcasts/src/app/home/home.component.ts
+++ cultpodcasts/src/app/home/home.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { IHomepage } from '../IHomepage';
 import { SiteService } from '../SiteService';
 import { IHomepageItem } from '../IHomepageItem';
 import { KeyValue } from '@angular/common';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { combineLatest } from 'rxjs/internal/observable/combineLatest';
 
+const pageSize:number = 10;
+const pageParam:string = "page";
 
 @Component({
   selector: 'app-home',
@@ -13,9 +17,21 @@ import { KeyValue } from '@angular/common';
 })
 export class HomeComponent {
   grouped: { [key: string]: IHomepageItem[]; } ;
-  constructor(private http: HttpClient, private siteService: SiteService) {
+  currentPage: number=1;
+  podcastCount: number|undefined;
+  constructor(private router: Router, private http: HttpClient, private siteService: SiteService) {
     this.grouped= {};
   }
+  private route = inject(ActivatedRoute);
+
+  prevPage: number=0;
+  nextPage: number=0;
+
+  isLoading: boolean= true;
+  showPagingPrevious:boolean=false;
+  showPagingPreviousInit: boolean= false;
+  showPagingNext:boolean=false;
+
 
   Weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
   Month = ["January","February","March","April","May","June","July", "August", "September", "October", "November", "December"]
@@ -42,19 +58,61 @@ export class HomeComponent {
   totalDuration: string="";
 
   ngOnInit() {
-    this.siteService.setQuery("");
-    let homepage= this.http.get<IHomepage>("https://api.cultpodcasts.com/homepage")
-      .subscribe(data=>{
-        this.homepage= data;
-        this.totalDuration= data.totalDuration.split(".")[0]+" days";
-        this.grouped= data.recentEpisodes.reduce((group: {[key: string]: IHomepageItem[]}, item)=>{
-          item.release= new Date(item.release);
-          if (!group[item.release.toLocaleDateString()]) {
-            group[item.release.toLocaleDateString()] = [];
-           }
-           group[item.release.toLocaleDateString()].push(item);
-           return group;
-        }, {});
+    combineLatest(
+      this.route.params,
+      this.route.queryParams,
+      (params: Params, queryParams: Params) => ({
+        params,
+        queryParams,
+      })
+    ).subscribe((res: { params: Params; queryParams: Params }) => {
+      const { params, queryParams} = res;
+
+      this.siteService.setQuery("");
+
+      this.currentPage=1;
+      if (queryParams[pageParam]) {
+        this.currentPage= parseInt(queryParams[pageParam]);
+        this.prevPage= this.currentPage - 1;
+        this.nextPage= this.currentPage + 1;
+      } else {
+        this.nextPage= 2;
+      }
+
+      let homepage= this.http.get<IHomepage>("https://api.cultpodcasts.com/homepage")
+        .subscribe(data=>{
+          this.homepage= data;
+          this.totalDuration= data.totalDuration.split(".")[0]+" days";
+          let start= (this.currentPage -1) * pageSize;
+          this.podcastCount= data.recentEpisodes.length;
+          var pageEpisodes= data.recentEpisodes.slice(start, start + pageSize);
+          this.grouped= pageEpisodes.reduce((group: {[key: string]: IHomepageItem[]}, item)=>{
+            item.release= new Date(item.release);
+            if (!group[item.release.toLocaleDateString()]) {
+              group[item.release.toLocaleDateString()] = [];
+            }
+            group[item.release.toLocaleDateString()].push(item);
+            return group;
+          }, {});
+          this.isLoading= false;
+
+
+          this.showPagingPrevious= this.currentPage > 2;
+          this.showPagingPreviousInit= this.currentPage == 2;
+          this.showPagingNext= (this.currentPage * pageSize) < this.homepage.recentEpisodes.length;
+    
+
+      });
     });
+  }
+
+  setPage(page:number){
+    var url= `/`;
+    this.currentPage+= page;
+    var params:Params= {};
+    if (this.currentPage > 1) {
+      params["page"]= this.currentPage;
+    }
+    this.router.navigate([url], {queryParams:params} );
   }
 }
