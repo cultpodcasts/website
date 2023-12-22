2ba994246c54e0f12796b7ed38a14186f95d0405
Jon Breen <jonbreen@outlook.com>
2023-12-22 00:13:53 +0000

    SEO changes Co-authored-by: Jon Breen <jonbreen@outlook.com>

--- cultpodcasts/src/app/app-routing.module.ts
+++ cultpodcasts/src/app/app-routing.module.ts
@@ -1,16 +1,42 @@
-import { NgModule } from '@angular/core';
+import { Injectable, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
 import { SearchComponent } from './search/search.component';
 import { PodcastComponent } from './podcast/podcast.component';
-import { RouterModule, Routes } from '@angular/router';
+import { ActivatedRouteSnapshot, Resolve, RouterModule, Routes } from '@angular/router';
 import { SubjectComponent } from './subject/subject.component';
 
+const siteTitle= "Cult Podcasts";
+
+@Injectable({ providedIn: 'root' })
+class QueryTitle implements Resolve<string> {
+  constructor() { }
+  resolve(route: ActivatedRouteSnapshot) {
+    return `${siteTitle} | Search | ${route.params['query']}`;
+  }
+}
+
+@Injectable({ providedIn: 'root' })
+class PodcastTitle implements Resolve<string> {
+  constructor() { }
+  resolve(route: ActivatedRouteSnapshot) {
+    return `${siteTitle} | Podcasts | ${route.params['podcastName']}`;
+  }
+}
+
+@Injectable({ providedIn: 'root' })
+class SubjectTitle implements Resolve<string> {
+  constructor() { }
+  resolve(route: ActivatedRouteSnapshot) {
+    return `${siteTitle} | Subject | ${route.params['subjectName']}`;
+  }
+}
+
 const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'search/:query', component: SearchComponent },
-  { path: 'podcast/:podcastName', component: PodcastComponent },
-  { path: 'subject/:subjectName', component: SubjectComponent },
+  { path: '', component: HomeComponent, title: "Cult Podcasts" },
+  { path: 'search/:query', component: SearchComponent, title: QueryTitle },
+  { path: 'podcast/:podcastName', component: PodcastComponent, title: PodcastTitle },
+  { path: 'subject/:subjectName', component: SubjectComponent, title: SubjectTitle },
 ];
 
 @NgModule({
@@ -18,3 +44,4 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
