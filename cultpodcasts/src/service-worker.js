36768ce7e866d18a876097cefeb95d576e1db840
Jon Breen <jonbreen@outlook.com>
2024-02-19 22:41:48 +0000

    Change sw

--- cultpodcasts/src/service-worker.js
+++ cultpodcasts/src/service-worker.js
@@ -2,16 +2,15 @@ self.addEventListener('fetch', event => {
     const url = new URL(event.request.url);
     if (event.request.method === 'POST' && url.pathname === '/share') {
         event.respondWith((async () => {
-            const formData = await event.request.formData();
+            const formData =  await event.request.formData();
             const link = formData.get('url') || '';
+            const title = formData.get('title') || '';
+            const text = formData.get('text') || '';
+            const clientId= event.clientId;
 
-            // Get the client.
-            const client = await self.clients.get(event.clientId);
-            // Exit early if we don't get the client.
-            // Eg, if it closed.
+            const client = await self.clients.get(clientId);
             if (!client) return Response.redirect("/?no-client", 511);
 
-            // Send a message to the client.
             client.postMessage({
                 msg: "podcast-share",
                 url: link,
