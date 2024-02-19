b1e64f50cbd8b470bbaa477b5882e392b495a0df
Jon Breen <jonbreen@outlook.com>
2024-02-19 23:19:56 +0000

    Use a different pattern

--- cultpodcasts/src/service-worker.js
+++ cultpodcasts/src/service-worker.js
@@ -1,21 +1,25 @@
 self.addEventListener('fetch', event => {
     const url = new URL(event.request.url);
     if (event.request.method === 'POST' && url.pathname === '/share') {
-        event.respondWith((async () => {
-            const formData =  await event.request.formData();
+
+
+        event.respondWith(Response.redirect('/'))
+
+        event.waitUntil(async function () {
+            const formData = await event.request.formData();
             const text = formData.get('text') || '';
-            const clientId= event.resultingClientId || event.clientId;
+            const clientId = event.resultingClientId || event.clientId;
 
-            const client = await self.clients.get(clientId);
-            if (!client) return Response.redirect("/?no-client", 511);
+            setTimeout(async () => {
+                const client = await self.clients.get(clientId);
+                if (!client) return Response.redirect("/?no-client", 511);
 
-            client.postMessage({
-                msg: "podcast-share",
-                url: text,
-            });
-
-            return Response.redirect("/", 303);
-        })());
+                client.postMessage({
+                    msg: "podcast-share",
+                    url: text,
+                });
+            }, 2000)
+        }())
     } else {
         return;
     }
