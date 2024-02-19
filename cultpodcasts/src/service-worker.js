e69c4e7ae7f5a83f01e8eee2ac49dbf8766f10fd
Jon Breen <jonbreen@outlook.com>
2024-02-19 22:17:10 +0000

    Reinstating event handler

--- cultpodcasts/src/service-worker.js
+++ cultpodcasts/src/service-worker.js
@@ -1,42 +1,29 @@
 self.addEventListener('fetch', event => {
-    // const url = new URL(event.request.url);
-    // if (event.request.method === 'POST' && url.pathname === '/share') {
-    //     event.respondWith((async () => {
-    //         const formData = await event.request.formData();
-    //         const link = formData.get('link') || '';
+    const url = new URL(event.request.url);
+    if (event.request.method === 'POST' && url.pathname === '/share') {
+        event.respondWith((async () => {
+            const formData = await event.request.formData();
+            const link = formData.get('url') || '';
 
-    //         if (!event.clientId) return Response.redirect("/no-client-id", 510);
+            // Get the client.
+            const client = await self.clients.get(event.clientId);
+            // Exit early if we don't get the client.
+            // Eg, if it closed.
+            if (!client) return Response.redirect("/?no-client", 511);
 
-    //         // Get the client.
-    //         const client = await self.clients.get(event.clientId);
-    //         // Exit early if we don't get the client.
-    //         // Eg, if it closed.
-    //         if (!client) return Response.redirect("/no-client", 511);
+            // Send a message to the client.
+            client.postMessage({
+                msg: "podcast-share",
+                url: link,
+            });
 
-    //         // Send a message to the client.
-    //         client.postMessage({
-    //             msg: "podcast-share",
-    //             url: link,
-    //         });
-
-    //         return Response.redirect("/", 303);
-    //     })());
-    // } else {
-    //     return Response.redirect("/", 512)
-    // }
-
-    if ((event.request.url.endsWith('/share')) && (event.request.method === 'POST')) {
-        return event.respondWith((async () => {
-            // This function is async.
-            const formData = await fetchEvent.request.formData();
-            // Do something with the URL…
-            const url = formData.get('url');
-            // Store the URL, process it, communicate it to the clients…
-            // You need to redirect the user somewhere, since the path
-            // /receive-shares does not actually exist.
-            return Response.redirect('/', 303);
-        })())
+            return Response.redirect("/", 303);
+        })());
+    } else {
+        return Response.redirect("/", 512)
     }
+
+
 });
 
 importScripts('./ngsw-worker.js');
