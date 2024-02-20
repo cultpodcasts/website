5e86a8f09b3aa279d45ce0ce225808e28b094465
Jon Breen <jonbreen@outlook.com>
2024-02-20 21:57:50 +0000

    Move sw unregister in app oninit

--- cultpodcasts/src/app/app.component.ts
+++ cultpodcasts/src/app/app.component.ts
@@ -43,6 +43,24 @@ export class AppComponent {
   }
 
   ngOnInit() {
+    navigator.serviceWorker.getRegistrations().then(function (registrations) {
+      if (!registrations.length) {
+        console.log('No serviceWorker registrations found.')
+        return
+      }
+      for (let registration of registrations) {
+        registration.unregister().then(function (boolean) {
+          console.log(
+            (boolean ? 'Successfully unregistered' : 'Failed to unregister'), 'ServiceWorkerRegistration\n' +
+            (registration.installing ? '  .installing.scriptURL = ' + registration.installing.scriptURL + '\n' : '') +
+            (registration.waiting ? '  .waiting.scriptURL = ' + registration.waiting.scriptURL + '\n' : '') +
+            (registration.active ? '  .active.scriptURL = ' + registration.active.scriptURL + '\n' : '') +
+            '  .scope: ' + registration.scope + '\n'
+          )
+        })
+      }
+    })
+
     this.siteService.currentSiteData.subscribe(siteData => {
       if (this.searchBox) {
         this.searchBox.nativeElement.value = siteData.query;
