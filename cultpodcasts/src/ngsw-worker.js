433a1353287bf2704f9a2d2c77acd90952e4a65c
Jon Breen <jonbreen@outlook.com>
2024-02-22 20:03:44 +0000

    Unregister sw

--- cultpodcasts/src/ngsw-worker.js
+++ cultpodcasts/src/ngsw-worker.js
@@ -1,25 +1,27 @@
-removeInitialSw();
 function removeInitialSw() {
     navigator.serviceWorker.getRegistrations().then(function (registrations) {
-      if (!registrations.length) {
-        console.log('No serviceWorker registrations found.')
-        return
-      }
-      for (let registration of registrations) {
-        if (registration.active?.scriptURL.endsWith("ngsw-worker.js") || registration.installing?.scriptURL.endsWith("ngsw-worker.js") || registration.waiting?.scriptURL.endsWith("ngsw-worker.js")) {
-          registration.unregister().then(function (boolean) {
-            console.log(
-              (boolean ? 'Successfully unregistered' : 'Failed to unregister'), 'ServiceWorkerRegistration\n' +
-              (registration.installing ? '  .installing.scriptURL = ' + registration.installing.scriptURL + '\n' : '') +
-              (registration.waiting ? '  .waiting.scriptURL = ' + registration.waiting.scriptURL + '\n' : '') +
-              (registration.active ? '  .active.scriptURL = ' + registration.active.scriptURL + '\n' : '') +
-              '  .scope: ' + registration.scope + '\n'
-            )
-          })
+        if (!registrations.length) {
+            console.log('No serviceWorker registrations found.')
+            return
         }
-      }
+        for (let registration of registrations) {
+            if (registration.active?.scriptURL.endsWith("ngsw-worker.js") || registration.installing?.scriptURL.endsWith("ngsw-worker.js") || registration.waiting?.scriptURL.endsWith("ngsw-worker.js")) {
+                registration.unregister().then(function (boolean) {
+                    console.log(
+                        (boolean ? 'Successfully unregistered' : 'Failed to unregister'), 'ServiceWorkerRegistration\n' +
+                        (registration.installing ? '  .installing.scriptURL = ' + registration.installing.scriptURL + '\n' : '') +
+                        (registration.waiting ? '  .waiting.scriptURL = ' + registration.waiting.scriptURL + '\n' : '') +
+                        (registration.active ? '  .active.scriptURL = ' + registration.active.scriptURL + '\n' : '') +
+                        '  .scope: ' + registration.scope + '\n'
+                    )
+                })
+            }
+        }
     })
-  }
+}
+
+removeInitialSw();
+
 
 (() => {
     var __defProp = Object.defineProperty;
