afcd14023b9e5265e8888fc109d58ebd70de8ea0
CultPodcastsBot <142722442+cultpodcasts@users.noreply.github.com>
2024-02-25 23:51:09 +0000

    www. prefix is optional on YT share urls (#70)
    Co-authored-by: Jon Breen <jonbreen@outlook.com>

--- cultpodcasts/src/app/send-podcast/send-podcast.component.ts
+++ cultpodcasts/src/app/send-podcast/send-podcast.component.ts
@@ -19,7 +19,7 @@ export class SendPodcastComponent {
   submitError: boolean = false;
   shareUrl: URL | undefined;
   spotify: RegExp = /https:\/\/open\.spotify\.com\/episode\/[A-Za-z\d]+/;
-  youtube: RegExp = /https:\/\/(?:www\.youtube\.com\/watch\?v=|youtu\.be\/)[A-Za-z\d\-]+/;
+  youtube: RegExp = /https:\/\/(?:(?:www\.)?youtube\.com\/watch\?v=|youtu\.be\/)[A-Za-z\d\-]+/;
   apple: RegExp = /https:\/\/podcasts\.apple\.com\/(\w+\/)?podcast\/[a-z\-0-9]+\/id\d+\?i=\d+/;
 
   constructor(
