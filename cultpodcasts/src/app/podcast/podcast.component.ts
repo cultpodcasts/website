e75bd51ebc08ee793b6fd8297f63a49dc3695cd7
Jon Breen <jonbreen@outlook.com>
2024-03-07 13:56:15 +0000

    Working version for podcasts

--- cultpodcasts/src/app/podcast/podcast.component.ts
+++ cultpodcasts/src/app/podcast/podcast.component.ts
@@ -5,15 +5,15 @@ import { combineLatest } from 'rxjs/internal/observable/combineLatest';
 import { SiteService } from '../SiteService';
 import { ISearchState } from '../ISearchState';
 import { ODataService } from '../OdataService'
-const pageSize:number = 10;
+const pageSize: number = 10;
 
-const sortParam:string = "sort";
-const pageParam:string = "page";
-const queryParam:string = "query";
-const filterParam:string = "filter";
+const sortParam: string = "sort";
+const pageParam: string = "page";
+const queryParam: string = "query";
+const filterParam: string = "filter";
 
-const sortParamDateAsc:string = "date-asc";
-const sortParamDateDesc:string = "date-desc";
+const sortParamDateAsc: string = "date-asc";
+const sortParamDateDesc: string = "date-desc";
 
 @Component({
   selector: 'app-podcast',
@@ -22,33 +22,33 @@ const sortParamDateDesc:string = "date-desc";
 })
 
 export class PodcastComponent {
-  searchState:ISearchState= {
-    query:"",
-    page:1,
-    sort:sortParamDateDesc,
-    filter:null
+  searchState: ISearchState = {
+    query: "",
+    page: 1,
+    sort: sortParamDateDesc,
+    filter: null
   }
 
-  podcastName: string="";
-  count: number=0;
+  podcastName: string = "";
+  count: number = 0;
 
-  prevPage: number=0;
-  nextPage: number=0;
-  
-  sortParamRank: string= sortParamDateDesc;
+  prevPage: number = 0;
+  nextPage: number = 0;
+
+  sortParamRank: string = sortParamDateDesc;
   sortParamDateAsc: string = sortParamDateAsc;
-  sortParamDateDesc : string = sortParamDateDesc;
+  sortParamDateDesc: string = sortParamDateDesc;
 
   constructor(private router: Router, private siteService: SiteService, private oDataService: ODataService) {
   }
   private route = inject(ActivatedRoute);
-  
+
   results: any;
-  resultsHeading: string= "";
-  isLoading: boolean= true;
-  showPagingPrevious:boolean=false;
-  showPagingPreviousInit: boolean= false;
-  showPagingNext:boolean=false;
+  resultsHeading: string = "";
+  isLoading: boolean = true;
+  showPagingPrevious: boolean = false;
+  showPagingPreviousInit: boolean = false;
+  showPagingNext: boolean = false;
 
   ngOnInit() {
     combineLatest(
@@ -59,39 +59,39 @@ export class PodcastComponent {
         queryParams,
       })
     ).subscribe((res: { params: Params; queryParams: Params }) => {
-      const { params, queryParams} = res;
+      const { params, queryParams } = res;
 
-      this.isLoading= true;
-      this.searchState.query= params["query"]??"";
+      this.isLoading = true;
+      this.searchState.query = params["query"] ?? "";
       this.siteService.setQuery(this.searchState.query);
-      this.podcastName= params["podcastName"];
+      this.podcastName = params["podcastName"];
       this.siteService.setPodcast(this.podcastName);
 
       if (queryParams[pageParam]) {
-        this.searchState.page= parseInt(queryParams[pageParam]);
-        this.prevPage= this.searchState.page - 1;
-        this.nextPage=  this.searchState.page + 1;
+        this.searchState.page = parseInt(queryParams[pageParam]);
+        this.prevPage = this.searchState.page - 1;
+        this.nextPage = this.searchState.page + 1;
       } else {
-        this.nextPage= 2;
-        this.searchState.page= 1;
+        this.nextPage = 2;
+        this.searchState.page = 1;
       }
 
       if (queryParams[sortParam]) {
-        this.searchState.sort= queryParams[sortParam];
+        this.searchState.sort = queryParams[sortParam];
       } else {
-        this.searchState.sort= sortParamDateDesc;
+        this.searchState.sort = sortParamDateDesc;
       }
 
 
-      this.searchState.filter= `(podcastName eq '${this.podcastName.replaceAll("'", "''")}')`;
+      this.searchState.filter = `(podcastName eq '${this.podcastName.replaceAll("'", "''")}')`;
       this.siteService.setFilter(this.searchState.filter);
 
-      let currentTime= Date.now();
-      var sort: string= "";
-      if (this.searchState.sort=="date-asc") {
-        sort= "release asc";
-      } else if (this.searchState.sort=="date-desc") {
-        sort= "release desc";
+      let currentTime = Date.now();
+      var sort: string = "";
+      if (this.searchState.sort == "date-asc") {
+        sort = "release asc";
+      } else if (this.searchState.sort == "date-desc") {
+        sort = "release desc";
       }
 
       this.oDataService.getEntities<ISearchResult>(
@@ -102,48 +102,51 @@ export class PodcastComponent {
           searchMode: 'any',
           queryType: 'simple',
           count: true,
-          skip:(this.searchState.page-1) * pageSize,
+          skip: (this.searchState.page - 1) * pageSize,
           top: pageSize,
           facets: ["podcastName,count:10,sort:count", "subjects,count:10,sort:count"],
           orderby: sort
-        }).subscribe(data=>{
-          this.results= data.entities;
-          var requestTime= (Date.now() - currentTime)/1000;
-          const count= data.metadata.get("count");
+        }).subscribe(data => {
+          this.results = data.entities;
+          var requestTime = (Date.now() - currentTime) / 1000;
+          const count = data.metadata.get("count");
 
-          this.count= count;
+          this.count = count;
 
-          this.isLoading= false;
-          this.showPagingPrevious= this.searchState.page!=undefined && this.searchState.page > 2;
-          this.showPagingPreviousInit= this.searchState.page!=undefined && this.searchState.page == 2;
-          this.showPagingNext= (this.searchState.page * pageSize) < count;
-        }, error=>{
-          this.resultsHeading= "Something went wrong. Please try again.";
-          this.isLoading= false;
+          this.isLoading = false;
+          this.showPagingPrevious = this.searchState.page != undefined && this.searchState.page > 2;
+          this.showPagingPreviousInit = this.searchState.page != undefined && this.searchState.page == 2;
+          this.showPagingNext = (this.searchState.page * pageSize) < count;
+        }, error => {
+          this.resultsHeading = "Something went wrong. Please try again.";
+          this.isLoading = false;
         });
     });
   }
 
   setSort(sort: string) {
-    var url= `/podcast/${this.podcastName}`;
-    var params:Params= {};
-    if (sort!=sortParamDateDesc) {
-      params[sortParam]= sort;
+    var url = `/podcast/${this.podcastName}`;
+    var params: Params = {};
+    if (sort != sortParamDateDesc) {
+      params[sortParam] = sort;
     }
-    this.router.navigate([url], {queryParams:params} );
-  }    
-  
-  setPage(page:number){
-    var url= `/podcast/${this.podcastName}`;
-    this.searchState.page+= page;
-    var params:Params= {};
-    if (this.searchState.page!=null && this.searchState.page > 1) {
-      params["page"]= this.searchState.page;
+    this.router.navigate([url], { queryParams: params });
+  }
+
+  setPage(page: number) {
+    var url = `/podcast/${this.podcastName}`;
+    if (this.searchState.query && this.searchState.query != "") {
+      url += "/" + this.searchState.query;
     }
-    if (this.searchState.sort!=sortParamDateDesc) {
-      params[sortParam]= this.searchState.sort;
+    this.searchState.page += page;
+    var params: Params = {};
+    if (this.searchState.page != null && this.searchState.page > 1) {
+      params["page"] = this.searchState.page;
     }
-    this.router.navigate([url], {queryParams:params} );
+    if (this.searchState.sort != sortParamDateDesc) {
+      params[sortParam] = this.searchState.sort;
+    }
+    this.router.navigate([url], { queryParams: params });
 
   }
 }
