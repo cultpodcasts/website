87578d61e092597bd5518a68c78a194e623cb28a
CultPodcastsBot <142722442+cultpodcasts@users.noreply.github.com>
2024-04-21 21:33:57 +0100

    Enable Auth0 for production (#80)
    Co-authored-by: Jon Breen <jonbreen@outlook.com>

--- cultpodcasts/src/environments/environment.ts
+++ cultpodcasts/src/environments/environment.ts
@@ -2,7 +2,7 @@ import { IEnvironment } from "./IEnvironment";
 
 export const environment:IEnvironment = {
    auth0: {
-    clientId: "",
-    domain: ""
+    clientId: "LbMkF8xiBGeuHGl7aBrzvkNuzWr3ryCt",
+    domain: "cultpodcasts.uk.auth0.com"
    }
 };
