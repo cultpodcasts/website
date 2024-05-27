1a2be59a9a5dcef26b2af16fb9d04d30adb40318
Jon Breen <jonbreen@outlook.com>
2024-05-27 14:34:57 +0100

    Improve chip width

--- cultpodcasts/src/app/app.component.ts
+++ cultpodcasts/src/app/app.component.ts
@@ -163,9 +163,10 @@ export class AppComponent {
   }
 
   searchChipDisplay() {
+    const width = screen.availWidth > 500 ? 30 : screen.availWidth > 400 ? 15 : 10;
     if (this.searchChip) {
-      if (this.searchChip.length > 10) {
-        return this.searchChip.substring(0, 9) + "…";
+      if (this.searchChip.length > width) {
+        return this.searchChip.substring(0, width - 1) + "…";
       } else {
         return this.searchChip;
       }
