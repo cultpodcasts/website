568c1071d25d7a89e3b8f77b6532aeee269745a8
Jon Breen <jonbreen@outlook.com>
2024-06-20 14:11:16 +0100

    probe build env

--- cultpodcasts/build.sh
+++ cultpodcasts/build.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
 echo ENV:
 echo "$(env)"
+
 node update-version.js
 cp src/environments/version.prod.ts src/environments/version.ts
+
+echo "npm run build --configuration $($env)"
 npx npm run build --configuration $env
 echo build complete
-echo PWD:
-echo "$(pwd)"
-echo LS:
-echo $(ls)
-echo LS dist:
-echo $(ls dist)
-echo LS dist/cloudflare:
-echo $(ls dist/cloudflare)
+
 cp ./dist/cloudflare/ngsw-worker.js ./dist/cloudflare/ngsw-worker-dist.js
 echo switched ngsw-worker.js -> ngsw-worker-dist.js
+
 cp ./src/remove-ngsw-worker.js ./dist/cloudflare/ngsw-worker.js
 echo switched remove-ngsw-worker.js -> ngsw-worker.js
