79205826fa99a9dbcf7e1ead7050f8cc6c443032
Jon Breen <jonbreen@outlook.com>
2024-06-20 13:36:14 +0100

    Fix build path

--- cultpodcasts/build.sh
+++ cultpodcasts/build.sh
@@ -3,7 +3,7 @@ node update-version.js
 cp src/environments/version.prod.ts src/environments/version.ts
 npx ng build --configuration $env
 echo build complete
-cp ./dist/cultpodcasts/cloudflare/ngsw-worker.js ./dist/cultpodcasts/cloudflare/ngsw-worker-dist.js
+cp ./dist/cloudflare/ngsw-worker.js ./dist/cloudflare/ngsw-worker-dist.js
 echo switched ngsw-worker.js -> ngsw-worker-dist.js
-cp ./src/remove-ngsw-worker.js ./dist/cultpodcasts/cloudflare/ngsw-worker.js
+cp ./src/remove-ngsw-worker.js ./dist/cloudflare/ngsw-worker.js
 echo switched remove-ngsw-worker.js -> ngsw-worker.js
