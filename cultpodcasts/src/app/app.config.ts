12d0d673c515f76e960d8491dc4cc8e35df592c4
Jon Breen <jonbreen@outlook.com>
2024-07-11 16:14:47 +0100

    Reworked DI to fox sw config

--- cultpodcasts/src/app/app.config.ts
+++ cultpodcasts/src/app/app.config.ts
@@ -1,19 +1,86 @@
-import { provideHttpClient, withFetch } from '@angular/common/http';
-import { ApplicationConfig, provideZoneChangeDetection, isDevMode } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection, isDevMode, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app-routing.module';
-import { provideClientHydration } from '@angular/platform-browser';
 import { provideServiceWorker } from '@angular/service-worker';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserModule, bootstrapApplication, provideClientHydration } from '@angular/platform-browser';
+import { JsonUrlInterceptor } from './JsonUrlInterceptor';
+import { JsonDateInterceptor } from './JsonDateInterceptor';
+import { HTTP_INTERCEPTORS, withInterceptorsFromDi, provideHttpClient, withFetch } from '@angular/common/http';
+import { environment } from '../environments/environment';
+import { provideAuth0 } from '@auth0/auth0-angular';
+import { SiteService } from './SiteService';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgIf } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideHttpClient(withFetch()),
+    importProvidersFrom(
+      BrowserModule,
+      AppRoutingModule,
+      MatInputModule,
+      MatIconModule,
+      MatButtonModule,
+      FormsModule,
+      ReactiveFormsModule,
+      NgIf,
+      MatCardModule,
+      MatExpansionModule,
+      MatToolbarModule,
+      MatProgressBarModule,
+      MatMenuModule,
+      MatDialogModule,
+      MatProgressSpinnerModule,
+      MatSnackBarModule,
+      MatChipsModule,
+      MatAutocompleteModule,
+      MatFormFieldModule,
+      MatDividerModule,
+      MatBadgeModule,
+      MatButtonToggleModule
+    ),
+    SiteService,
+    provideAuth0({
+      domain: environment.auth0.domain,
+      clientId: environment.auth0.clientId,
+      authorizationParams: {
+        redirect_uri: environment.assetHost
+      }
+    }),
+    {
+      provide: HTTP_INTERCEPTORS, useClass: JsonDateInterceptor, multi: true
+    },
+    {
+      provide: HTTP_INTERCEPTORS, useClass: JsonUrlInterceptor, multi: true
+    },
+    provideHttpClient(withFetch(), withInterceptorsFromDi()),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideClientHydration(),
     provideServiceWorker('service-worker.js', {
       enabled: !isDevMode(),
       registrationStrategy: 'registerImmediately' //'registerWhenStable:30000'
-    }
-    )]
+    }),
+    provideAnimations(),
+    provideAnimationsAsync(),
+  ]
 };
