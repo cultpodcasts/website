9eb54fabd751a9012ba13cc75f55a64c09232d50
CultPodcastsBot <142722442+cultpodcasts@users.noreply.github.com>
2024-08-06 22:23:11 +0100

    Feature/edit-subject (#136)
    * Add edit-subject button and componeent
    
    * Added form handling for subject
    
    * Handle nulls in subject dto

---
+++ cultpodcasts/src/app/subject-form.ts
@@ -0,0 +1,14 @@
+import { FormControl } from "@angular/forms";
+
+export interface SubjectForm {
+    name: FormControl<string>;
+    aliases: FormControl<string[] | null | undefined>;
+    associatedSubjects: FormControl<string[] | null | undefined>;
+    subjectType: FormControl<string | null | undefined>;
+
+    enrichmentHashTags: FormControl<string[] | null | undefined>;
+    hashTag: FormControl<string | null | undefined>;
+
+    redditFlairTemplateId: FormControl<string | null | undefined>;
+    redditFlareText: FormControl<string | null | undefined>;
+}
