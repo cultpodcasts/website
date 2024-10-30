5dd154debf26fa38394b73dd97fd75eb3b2c4512
Jon Breen <jonbreen@outlook.com>
2024-10-30 18:58:24 +0000

    Remove custom service-worker handler for notifications

---
+++ cultpodcasts/src/ngsw-worker-dist.js
@@ -0,0 +1,1 @@
+// dummy file for local-dev
--- cultpodcasts/src/service-worker.js
+++ cultpodcasts/src/service-worker.js
@@ -19,15 +19,4 @@ self.addEventListener('fetch', event => {
     }
 });
 
-self.addEventListener('notificationclick', (event) => {
-    event.notification.close();
-    if (clients.openWindow) {
-        if (event.notification && event.notification.data && event.notification.data.url) {
-            event.waitUntil(clients.openWindow(event.notification.data.url));
-        } else {
-            event.waitUntil(clients.openWindow("/"));
-        }
-    }
-});
-
 importScripts('./ngsw-worker-dist.js');
