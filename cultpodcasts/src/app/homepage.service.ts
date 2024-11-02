42c427e2e10e3cf4ed483a35ebc1866978cb887b
Jon Breen <jonbreen@outlook.com>
2024-11-02 22:28:30 +0000

    Log

--- cultpodcasts/src/app/homepage.service.ts
+++ cultpodcasts/src/app/homepage.service.ts
@@ -25,40 +25,54 @@ export class HomepageService {
   getHomepage(): Promise<IHomepage> {
     var homepageData: IHomepage | undefined;
     if (this.isBrowser) {
+      console.log("-1");
       firstValueFrom(this.http.get<IHomepage>(new URL("/homepage", environment.api).toString()))
         .then(d => homepageData = d)
         .catch(e => Promise.reject(e));
     } else if (this.isServer) {
+      console.log("-2");
       this.contentBucket.get("homepage")
         .then(x => {
+          console.log("-3");
           if (!x) {
+            console.log("-4");
             return Promise.reject("No homepage data");
           }
           else {
+            console.log("-5");
             console.log("got homepage data. deserialising")
             x.text().then(d => {
               console.log(d)
               return Promise.resolve(JSON.parse(d) as IHomepage)
             })
               .catch(e => {
+                console.log("-6");
                 console.log(e);
                 return Promise.reject(e);
               });
           }
+          console.log("-7");
           return Promise.reject("unknown")
         })
-        .then(data => homepageData = data)
+        .then(data => {
+          console.log("-8");
+          homepageData = data
+        })
         .catch(e => {
+          console.log("-9");
           console.log("caught " + e)
           Promise.reject(e)
         });
     } else {
+      console.log("-10");
       console.log("unknown platform")
       Promise.reject('Unknown platform.');
     }
     if (!homepageData) {
+      console.log("-11");
       return Promise.reject("unable to obtain homepage-data");
     }
+    console.log("-12");
     return Promise.resolve(homepageData);
   }
 }
