39950bf2f81f1109a8c1f23171593ee13249180d
Jon Breen <jonbreen@outlook.com>
2024-11-03 17:04:58 +0000

    Push homepage into deferable view

--- cultpodcasts/src/app/homepage.service.ts
+++ cultpodcasts/src/app/homepage.service.ts
@@ -10,50 +10,28 @@ import { R2Bucket } from '@cloudflare/workers-types';
   providedIn: 'root'
 })
 export class HomepageService {
-  isBrowser: boolean;
-  isServer: boolean;
   constructor(
     private http: HttpClient,
-    @Inject(PLATFORM_ID) private platformId: any,
     @Optional() @Inject('content') private contentBucket: R2Bucket
+  ) { }
 
-  ) {
-    this.isBrowser = isPlatformBrowser(platformId);
-    this.isServer = isPlatformServer(platformId);
+  async getHomepageFromR2(): Promise<IHomepage> {
+    var r2Obj = await this.contentBucket.get("homepage");
+    if (r2Obj) {
+      var r2Json = await r2Obj?.text();
+      if (r2Json) {
+        return JSON.parse(r2Json) as IHomepage;
+      } else {
+        throw new Error("No homepage text");
+      }
+    } else {
+      console.log("-6");
+      throw new Error("No homepage object");
+    }
   }
 
-  async getHomepage(): Promise<IHomepage> {
-    var homepageData: IHomepage | undefined;
-    if (this.isBrowser) {
-      console.log("-1")
-      return await firstValueFrom(this.http.get<IHomepage>(new URL("/homepage", environment.api).toString()));
-    } else if (this.isServer) {
-      console.log("-2");
-      try {
-        console.log("-2.5");
-        var r2Obj = await this.contentBucket.get("homepage");
-        if (r2Obj) {
-          console.log("-3");
-          var r2Json = await r2Obj?.text();
-          if (r2Json) {
-            console.log("-4");
-            return JSON.parse(r2Json) as IHomepage;
-          } else {
-            console.log("-5");
-            throw new Error("No homepage text");
-          }
-        } else {
-          console.log("-6");
-          throw new Error("No homepage object");
-        }
-      } catch (e) {
-        console.log("error")
-        console.log(e)
-        throw e;
-      }
-    }
-    console.log("-7");
-    return Promise.reject("Unable to obtain homepage");
+  async getHomepageFromApi(): Promise<IHomepage> {
+    return await firstValueFrom(this.http.get<IHomepage>(new URL("/homepage", environment.api).toString()));
   }
 }
 
