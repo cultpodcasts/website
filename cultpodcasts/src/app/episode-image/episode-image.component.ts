3b82ea6f2e346f1d18d9ed3b122388291bb97e38
Jon Breen <jonbreen@outlook.com>
2024-12-04 14:09:14 +0000

    Crop YT letter boxed images

--- cultpodcasts/src/app/episode-image/episode-image.component.ts
+++ cultpodcasts/src/app/episode-image/episode-image.component.ts
@@ -1,12 +1,23 @@
+import { NgClass } from '@angular/common';
 import { Component, Input, input } from '@angular/core';
 
 @Component({
   selector: 'app-episode-image',
-  imports: [],
+  imports: [NgClass],
   templateUrl: './episode-image.component.html',
   styleUrl: './episode-image.component.sass'
 })
 export class EpisodeImageComponent {
   @Input()
   imageUrl: URL | undefined | string;
+
+  get isCropped(): boolean {
+    if (this.imageUrl) {
+      let url = this.imageUrl.toString();
+      if (url.indexOf("https://i.ytimg.com") == 0 && url.indexOf("maxresdefault") == -1) {
+        return true;
+      }
+    }
+    return false;
+  }
 }
