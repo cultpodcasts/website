e041bc7f09d240c2f86027b3177f1fe957e6799d
Jon Breen <jonbreen@outlook.com>
2024-12-10 23:04:37 +0000

    Bump version

--- cultpodcasts/src/environments/environment.ts
+++ cultpodcasts/src/environments/environment.ts
@@ -7,7 +7,7 @@ export const environment: IEnvironment = {
    },
    api: 'https://127.0.0.1:8787',
    assetHost: 'https://local.cultpodcasts.com:4200/',
-   bundleAssetHost: 'https://local.cultpodcasts.com:4200',
+   bundleAssetHost: 'https://ee38295e.website-83e.pages.dev',
    shortner: 'https://s.cultpodcasts.com',
    name: "dev",
    vapidPublicKey: "BKx7EI56y8biaGTAo_bagpNPTR9f4AkHqtuUoHaRM7nNduX5ExbAHO74-YAKa6_c9wLVYWHZklhrpPl6Bbx_3Is",
