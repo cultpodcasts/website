563ccbea53b04e1f44cc4e8b1bcaa1df9c092511
Jon Breen <jonbreen@outlook.com>
2024-12-20 15:56:48 +0000

    Page bookmarks

--- cultpodcasts/src/app/bookmarks-api/bookmarks-api.component.ts
+++ cultpodcasts/src/app/bookmarks-api/bookmarks-api.component.ts
@@ -6,7 +6,7 @@ import { AuthServiceWrapper } from '../AuthServiceWrapper';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { environment } from './../../environments/environment';
 import { Episode } from '../episode';
-import { NgClass, DatePipe } from '@angular/common';
+import { DatePipe } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
@@ -23,6 +23,7 @@ import { PostEpisodeModel } from '../post-episode-model';
 import { EpisodePublishResponseAdaptor } from '../episode-publish-response-adaptor';
 import { BookmarkComponent } from "../bookmark/bookmark.component";
 import { SubjectsComponent } from "../subjects/subjects.component";
+import { ScrollDispatcher, ScrollingModule } from '@angular/cdk/scrolling';
 
 export enum sortMode {
   addDatedAsc = 1,
@@ -38,35 +39,39 @@ export enum sortMode {
     MatButtonModule,
     MatMenuModule,
     MatIconModule,
-    NgClass,
     MatCardModule,
     RouterLink,
     DatePipe,
     EpisodePodcastLinksComponent,
     EpisodeImageComponent,
     BookmarkComponent,
-    SubjectsComponent
-],
+    SubjectsComponent,
+    ScrollingModule
+  ],
   templateUrl: './bookmarks-api.component.html',
   styleUrl: './bookmarks-api.component.sass'
 })
 export class BookmarksApiComponent {
   isLoading: boolean = true;
   error: boolean = false;
-  private episodes: Episode[] = [];
-  protected sortedEpisodes: Episode[] = [];
+  private episodes: Set<Episode> = new Set<Episode>();
+  protected episodeItems: Episode[] = [];
   sortDirection: sortMode = sortMode.addDatedDesc;
   protected sortMode = sortMode;
   authRoles: string[] = [];
   isSignedIn: boolean = false;
   noBookmarks: boolean = false;
-  
+  private take: number = 3;
+  private page: number = 0;
+  bookmarks: Set<string> = new Set<string>();
+
   constructor(
     private profileService: ProfileService,
     private auth: AuthServiceWrapper,
     private http: HttpClient,
     private dialog: MatDialog,
-    private snackBar: MatSnackBar
+    private snackBar: MatSnackBar,
+    private scrollDisplatcher: ScrollDispatcher
   ) {
     this.auth.roles.subscribe(roles => this.authRoles = roles);
     this.auth.isSignedIn.subscribe(isSignedIn => this.isSignedIn = isSignedIn);
@@ -77,46 +82,66 @@ export class BookmarksApiComponent {
   }
 
   async populatePage() {
-    this.profileService.bookmarks$.subscribe(bookmarks => {
-      if (bookmarks.size > 0) {
-        this.noBookmarks = false;
-        var token = firstValueFrom(this.auth.authService.getAccessTokenSilently({
-          authorizationParams: {
-            audience: `https://api.cultpodcasts.com/`,
-            scope: 'curate'
-          }
-        }));
-        token.then(_token => {
-          let headers: HttpHeaders = new HttpHeaders();
-          headers = headers.set("Authorization", "Bearer " + _token);
-
-          const episodeResponses: Observable<Episode | null>[] = [];
-          bookmarks.forEach(episodeId => {
-            const episodeEndpoint = new URL(`/public/episode/${episodeId}`, environment.api).toString();
-            const get = this.http.get<Episode>(episodeEndpoint, { headers: headers }).pipe(this.handleRequest(this).bind(this))
-            episodeResponses.push(get);
-          })
-          forkJoin(episodeResponses).subscribe({
-            next: episodes => {
-              this.episodes = episodes.filter(x => x != null);
-              this.setSort(this.sortDirection);
-              this.isLoading = false;
-            },
-            error: e => {
-              this.error = true;
-              this.isLoading = false;
-              console.error(e);
-            }
-          })
-        });
-      } else {
-        this.error = false;
-        this.isLoading = false;
-        this.noBookmarks = true;
-        this.episodes = [];
-        this.sortedEpisodes= [];
-      }
+    this.profileService.bookmarks$.subscribe(async bookmarks => {
+      this.bookmarks = bookmarks;
+      await this.batch(true);
     });
+  }
+
+  async batch(first: boolean = false) {
+    if (this.bookmarks.size > 0) {
+      this.noBookmarks = false;
+      var token = firstValueFrom(this.auth.authService.getAccessTokenSilently({
+        authorizationParams: {
+          audience: `https://api.cultpodcasts.com/`,
+          scope: 'curate'
+        }
+      }));
+      token.then(_token => {
+        let headers: HttpHeaders = new HttpHeaders();
+        headers = headers.set("Authorization", "Bearer " + _token);
+
+        const episodeResponses: Observable<Episode | null>[] = [];
+        const start = this.page * this.take;
+        const end = start + this.take;
+        if (start >= this.bookmarks.size) {
+          return;
+        }
+        Array.from(this.bookmarks).slice(start, end).forEach(episodeId => {
+          const episodeEndpoint = new URL(`/public/episode/${episodeId}`, environment.api).toString();
+          const get = this.http.get<Episode>(episodeEndpoint, { headers: headers }).pipe(this.handleRequest(this).bind(this))
+          episodeResponses.push(get);
+        })
+        forkJoin(episodeResponses).subscribe({
+          next: episodes => {
+            (episodes.filter(x => x != null)).forEach(item => this.episodes.add(item));
+            this.episodeItems = {...Array.from(this.episodes)};
+            console.log("this.episodeItems size", this.episodeItems.length, this.episodeItems);
+
+            this.isLoading = false;
+
+            if (first && this.bookmarks.size > this.take) {
+              this.scrollDisplatcher.scrolled().subscribe(async () => {
+                if (this.isScrolledToBottom()) {
+                  console.log("scrolled to bottom");
+                  this.page++;
+                  await this.batch();
+                }
+              });
+            }
+          },
+          error: e => {
+            this.error = true;
+            this.isLoading = false;
+            console.error(e);
+          }
+        })
+      });
+    } else {
+      this.error = false;
+      this.isLoading = false;
+      this.noBookmarks = true;
+    }
   }
 
   handleRequest(that: any) {
@@ -170,51 +195,10 @@ export class BookmarksApiComponent {
     });
   }
 
-  setSort(mode: sortMode) {
-    var episodes: Episode[] = [];
-    this.episodes.forEach(val => episodes.push(Object.assign({}, val)));
-    switch (mode) {
-      case sortMode.addDatedAsc: {
-        this.sortedEpisodes = episodes;
-        break;
-      }
-      case sortMode.addDatedDesc: {
-        this.sortedEpisodes = episodes.reverse();
-        break;
-      }
-      case sortMode.releaseDateAsc: {
-        this.sortedEpisodes = episodes.sort((n1, n2) => {
-          let n1Release = n1.release.getTime();
-          let n2Release = n2.release.getTime();
-          if (n1Release > n2Release) {
-            return 1;
-          }
-          if (n1Release < n2Release) {
-            return -1;
-          }
-          return 0;
-        })
-        break;
-      };
-      case sortMode.releaseDateDesc: {
-        this.sortedEpisodes = episodes.sort((n1, n2) => {
-          let n1Release = n1.release.getTime();
-          let n2Release = n2.release.getTime();
-          if (n1Release > n2Release) {
-            return -1;
-          }
-          if (n1Release < n2Release) {
-            return 1;
-          }
-          return 0;
-        })
-        break;
-      };
-      default: {
-        console.error("Unhandled sort-mode", mode);
-        break;
-      }
-    }
-    this.sortDirection = mode;
+  private isScrolledToBottom(): boolean {
+    const scrollPosition = window.scrollY + window.innerHeight;
+    const threshold = document.documentElement.scrollHeight - 1;
+    return scrollPosition >= threshold;
   }
+
 }
