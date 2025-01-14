446e7794d974f64d62ca44a8b2deb727b2335ebd
Jon Breen <jonbreen@outlook.com>
2025-01-14 19:22:06 +0000

    Update bundleAssetHost URL in staging environment configuration

--- cultpodcasts/src/environments/environment.staging.ts
+++ cultpodcasts/src/environments/environment.staging.ts
@@ -7,7 +7,7 @@ export const environment: IEnvironment = {
    },
    api: 'https://api.cultpodcasts.com',
    assetHost: 'https://34c3225f.website-83e.pages.dev',
-   bundleAssetHost: 'https://34c3225f.website-83e.pages.dev',
+   bundleAssetHost: 'https://de61a149.website-83e.pages.dev',
    shortner: 'https://s.cultpodcasts.com',
    name: "stg",
    vapidPublicKey: "BKx7EI56y8biaGTAo_bagpNPTR9f4AkHqtuUoHaRM7nNduX5ExbAHO74-YAKa6_c9wLVYWHZklhrpPl6Bbx_3Is"
