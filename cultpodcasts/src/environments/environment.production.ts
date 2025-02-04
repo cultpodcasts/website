1fe1012a787b765cd1b7a0d70a3a80687c50ec5d
Jon Breen <jonbreen@outlook.com>
2025-02-04 18:43:43 +0000

    Update prod

--- cultpodcasts/src/environments/environment.production.ts
+++ cultpodcasts/src/environments/environment.production.ts
@@ -3,7 +3,7 @@ import { IEnvironment } from "./IEnvironment";
 export const environment: IEnvironment = {
    auth0: {
       clientId: "LbMkF8xiBGeuHGl7aBrzvkNuzWr3ryCt",
-      domain: "cultpodcasts.uk.auth0.com"
+      domain: "auth.cultpodcasts.com"
    },
    api: 'https://api.cultpodcasts.com',
    assetHost: 'https://cultpodcasts.com',
