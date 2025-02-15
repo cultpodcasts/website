59c7c9dcc05e6f34beb4ebbb9a2baa4a6ce0a90d
Jon Breen <jonbreen@outlook.com>
2025-02-15 15:27:31 +0000

    Update staging host

--- cultpodcasts/src/environments/environment.staging.ts
+++ cultpodcasts/src/environments/environment.staging.ts
@@ -6,8 +6,8 @@ export const environment: IEnvironment = {
       domain: "cultpodcasts-staging.uk.auth0.com"
    },
    api: 'https://api.cultpodcasts.com',
-   assetHost: 'https://34c3225f.website-83e.pages.dev',
-   bundleAssetHost: 'https://de61a149.website-83e.pages.dev',
+   assetHost: 'https://feature-api-caching.website-83e.pages.dev',
+   bundleAssetHost: 'https://feature-api-caching.website-83e.pages.dev',
    shortner: 'https://s.cultpodcasts.com',
    name: "stg",
    vapidPublicKey: "BKx7EI56y8biaGTAo_bagpNPTR9f4AkHqtuUoHaRM7nNduX5ExbAHO74-YAKa6_c9wLVYWHZklhrpPl6Bbx_3Is"
