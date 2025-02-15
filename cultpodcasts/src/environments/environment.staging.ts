e91d646d6f5d4c0fa40bee87771063ce8684129d
CultPodcastsBot <142722442+cultpodcasts@users.noreply.github.com>
2025-02-15 15:55:20 +0000

    Api caching (#314)
    * Api caching
    
    * Update staging host
    
    * Change auth-host

--- cultpodcasts/src/environments/environment.staging.ts
+++ cultpodcasts/src/environments/environment.staging.ts
@@ -3,11 +3,11 @@ import { IEnvironment } from "./IEnvironment";
 export const environment: IEnvironment = {
    auth0: {
       clientId: "o1YtyIzzhBEQ7c7IEZzovb1TGov1AGSs",
-      domain: "cultpodcasts-staging.uk.auth0.com"
+      domain: "auth-staging.cultpodcasts.com"
    },
    api: 'https://api.cultpodcasts.com',
-   assetHost: 'https://34c3225f.website-83e.pages.dev',
-   bundleAssetHost: 'https://de61a149.website-83e.pages.dev',
+   assetHost: 'https://feature-api-caching.website-83e.pages.dev',
+   bundleAssetHost: 'https://feature-api-caching.website-83e.pages.dev',
    shortner: 'https://s.cultpodcasts.com',
    name: "stg",
    vapidPublicKey: "BKx7EI56y8biaGTAo_bagpNPTR9f4AkHqtuUoHaRM7nNduX5ExbAHO74-YAKa6_c9wLVYWHZklhrpPl6Bbx_3Is"
