a8ca9999cfc8e110a3174d320287c40efa169d2c
MSI\jonbr <cultpodcasts@educocult.com>
2026-02-07 23:23:24 +0000

    Fix minimum duration change detection in podcast dialogs

--- cultpodcasts/src/app/edit-podcast-dialog/edit-podcast-dialog.component.ts
+++ cultpodcasts/src/app/edit-podcast-dialog/edit-podcast-dialog.component.ts
@@ -239,7 +239,7 @@ export class EditPodcastDialogComponent {
     if (!this.isSameA(prev.ignoredSubjects, now.ignoredSubjects)) changes.ignoredSubjects = now.ignoredSubjects;
     if (!this.areEqual(prev.lang ?? "unset", now.lang ?? "unset")) changes.lang = now.lang == "unset" ? "" : now.lang ?? "";
     if (!this.isSameA(prev.knownTerms, now.knownTerms)) changes.knownTerms = now.knownTerms;
-    if (prev.minimumDuration != now.minimumDuration) changes.minimumDuration = now.minimumDuration;
+    if ((prev.minimumDuration ?? "") != (now.minimumDuration ?? "")) changes.minimumDuration = now.minimumDuration ?? "";
     return changes;
   }
 
